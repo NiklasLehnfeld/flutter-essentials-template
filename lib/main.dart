75ae6d33a2e009ff911347e4b5e546f37563b94f
Niklas Lehnfeld <niklas@lehnfeld.net>
2020-07-31 23:57:31 +0200

    added localization functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:quiz_app_naturopath/localization/app_localizations.dart';
 import 'package:quiz_app_naturopath/routes/first_start_route.dart';
 
 void main() => runApp(MyApp());
@@ -7,11 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Template',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       initialRoute: '/',
       routes: {'/': (context) => FirstStartRoute()},
     );
--- lib/routes/first_start_route.dart
+++ lib/routes/first_start_route.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_naturopath/localization/app_localizations.dart';
 
 class FirstStartRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        constraints: BoxConstraints.expand(),
         color: Theme.of(context).primaryColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Theme.of(context)
+                    .textTheme
+                    .headline3
+                    .withLabel(context, 'sign_in'),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Theme.of(context)
+                    .textTheme
+                    .headline3
+                    .withLabel(context, 'sign_up'),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+extension TextStyleExt on TextStyle {
+  Widget withLabel(BuildContext context, String key) {
+    return Text(
+      AppLocalizations.of(context).translate(key),
+      style: this,
     );
   }
 }
